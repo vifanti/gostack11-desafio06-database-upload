e62d999c465f196baae2729b03ef71a538ada56b
Vinicius Fantinatto <vifanti@gmail.com>
2020-04-21 15:01:37 -0300

    Desafio Finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,57 @@
 // import AppError from '../errors/AppError';
 
+import { getRepository, getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+      if (value > total) {
+        throw new AppError('No enough cash, stranger...');
+      }
+    }
+
+    let checkCategoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategoryExists) {
+      checkCategoryExists = categoryRepository.create({ title: category });
+
+      await categoryRepository.save(checkCategoryExists);
+    }
+
+    const { id: category_id } = checkCategoryExists;
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
